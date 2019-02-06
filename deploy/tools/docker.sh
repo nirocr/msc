cf8d285721cad2d8908ceef5bc4c882bca7246a6
Niroshan Rajadurai <nirocr@gmail.com>
2019-02-06 07:54:49 +0000

    added docker setup, and chnaged permissions

---
+++ deploy/tools/docker.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
+sudo usermod -aG docker ${USER}
+su - ${USER}
+id -nG
+#sudo usermod -aG docker usernamedocker
+docker info
+docker run hello-world
+docker search ubuntu
+docker pull ubuntu
+docker images
+docker run -it ubuntu
